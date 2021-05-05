40cd6c478ba2642b1a9d1af8a2385e055b3c0761
Dean Davidson <ddavidson85@gmail.com>
2021-05-05 09:21:01 -0700

    Consolidate and add test data

--- format/format.go
+++ format/format.go
@@ -2,6 +2,16 @@ package format
 
 import "os"
 
+// Readable is an interface for file types that can be read from.
+type Readable interface {
+	// Initialize should initialize a encoding/<format> reader from the underlying file.
+	Initialize(file *os.File) error
+	// ReadHeader should return a list of column names.
+	ReadHeader() ([]string, error)
+	// ReadRow should read a single row and return it as a map[string]string where column name is key and column value is value.
+	ReadRow(headers []string) (map[string]string, error)
+}
+
 // Writable is for file types that can be written to.
 type Writable interface {
 	// WriteHeader should be called at the beginning of the file write process.
